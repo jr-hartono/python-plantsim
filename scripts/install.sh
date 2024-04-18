24c5e295cdac0602e4f95f27f10e6432b81aabeb
jr-hartono <130978844+jr-hartono@users.noreply.github.com>
2024-04-18 16:55:18 +0200

    - migrate to pyproject.toml - add install script - add ruff as dev dependency and its config

---
+++ scripts/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd "$(git rev-parse --show-toplevel)" || (
+  echo "Couldn't find project folder. Please check your working dir."
+  exit 1
+)
+
+pip install --upgrade --upgrade-strategy eager -e .[dev]
